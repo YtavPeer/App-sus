b3d9c8b256f9eaad9007b55af532bb0e0cd2f749
Michael Peer <michaelpeer2004@gmail.com>
2021-02-25 20:12:38 +0200

    header

--- app/app-cmps/app-header.cmp.js
+++ app/app-cmps/app-header.cmp.js
@@ -3,7 +3,7 @@ import appSearch from '../app-cmps/app-search.cmp.js'
 export default {
       name: 'app-header',
       template: `
-     <header class="app-header flex space-between" >
+     <!-- <header class="app-header flex space-between" >
             <section class="logo">
                   <h1>Books</h1>
             </section>
@@ -17,7 +17,22 @@ export default {
                   <router-link to="/book">books</router-link>|
 
            </nav>
-      </header>
+      </header> -->
+
+
+      <header class="main-header ">
+        <div class="header-container main-container flex space-between center-align">
+            <div class="logo">Books</div>
+            <app-search></app-search>
+            <ul class="navbar-conntainer clean-list flex ">
+            <router-link class="flex center-align nav-item" to="/">Home</router-link>
+                  <router-link class="flex center-align nav-item" to="/keep">keep</router-link>
+                  <router-link class="flex center-align nav-item" to="/email">mail</router-link>
+                  <router-link  class="flex center-align nav-item" to="/book">books</router-link>
+            </ul>
+            <!-- <button  onclick="toggleMenu()" class="menu-btn">&#9776</button> -->
+        </div>
+    </header>
       `,
       components: {
             appSearch
