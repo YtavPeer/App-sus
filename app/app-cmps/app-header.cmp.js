ca626153b75b97e17b614bc39f2998548e55e12f
Michael Peer <michaelpeer2004@gmail.com>
2021-02-26 16:42:26 +0200

    mobile menu

--- app/app-cmps/app-header.cmp.js
+++ app/app-cmps/app-header.cmp.js
@@ -20,7 +20,7 @@ export default {
       </header> -->
 
 
-      <header class="main-header ">
+      <header class="main-header" :class="menuState">
         <div class="header-container main-container flex space-between center-align">
             <div class="logo">APPSUS</div>
             <!-- <img src="../../img/logo.jpg" alt="" height=70px> -->
@@ -28,14 +28,32 @@ export default {
             <app-search></app-search>
             <ul class="navbar-conntainer clean-list flex ">
             <router-link class="flex center-align nav-item" to="/">Home</router-link>
-                  <router-link class="flex center-align nav-item" to="/keep">keep</router-link>
-                  <router-link class="flex center-align nav-item" to="/email">mail</router-link>
-                  <router-link  class="flex center-align nav-item" to="/book">books</router-link>
+                  <router-link @click.native="toggleMenu" class="flex center-align nav-item" to="/keep">keep</router-link>
+                  <router-link @click.native="toggleMenu" class="flex center-align nav-item" to="/email">mail</router-link>
+                  <router-link @click.native="toggleMenu"  class="flex center-align nav-item" to="/book">books</router-link>
             </ul>
+            <button  @click="toggleMenu" class="menu-btn" >&#9776</button>
             <!-- <button  onclick="toggleMenu()" class="menu-btn">&#9776</button> -->
         </div>
     </header>
       `,
+      data(){
+            return {
+                  isMenuOpen: false
+            }
+      },
+      methods: {
+            toggleMenu() {
+                  this.isMenuOpen = !this.isMenuOpen
+            }
+      },
+      computed: {
+            menuState() {
+                  return {
+                        'menu-open': this.isMenuOpen
+                  }
+            }
+      },
       components: {
             appSearch
       }
