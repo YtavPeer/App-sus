25b2c3821b88fad65bcc299d910c36f7702d2daf
YtavPeer <ytav.peer@gmail.com>
2021-02-27 22:01:39 +0200

    vytav5

--- app/keep/cmps/cmps-Notes/note-video.cmp.js
+++ app/keep/cmps/cmps-Notes/note-video.cmp.js
@@ -3,7 +3,7 @@ export default {
      props: ['dynamicNote'],
      template: `
               <section class="note-video">
-              <img  src="./img/videoicon.png" alt="" width=25>
+              <img  src="./img/videoIcon.png" alt="" width=25>
               <hr class="article-icon" style="height:1px;border:none;color:#ccc;background-color:#ccc;" />
                     <h2 class="note-title">{{dynamicNote.info.title}}</h2>
                      <video class="keep-video" width="100%" height="180" controls>
